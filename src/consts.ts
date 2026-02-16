8b2fed188b4e724b4763fac9ab1035de0c14f601
Rosario Fernandes <rosariopf@google.com>
2026-02-16 17:25:58 +0000

    Antigravity generated changes for pt landing page

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,8 @@
 export const SITE_TITLE = 'thatfire.dev';
 export const SITE_AUTHOR = 'Rosário P. Fernandes'
 export const PORTFOLIO_TITLE = SITE_AUTHOR + ' | Portfolio';
+export const PORTFOLIO_TITLE_PT = SITE_AUTHOR + ' | Portfólio';
 export const BLOG_TITLE = SITE_AUTHOR + ' | Blog';
+export const BLOG_TITLE_PT = SITE_AUTHOR + ' | Blog';
 export const SITE_DESCRIPTION = 'DevRel Engineer at Firebase';
+export const SITE_DESCRIPTION_PT = 'Engenheiro de DevRel no Firebase';
