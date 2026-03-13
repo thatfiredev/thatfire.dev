76774b5639c73c50aa9322e83dc0128a72884594
Rosario Fernandes <rosariopf@google.com>
2026-03-13 15:23:06 +0000

    chore: upgrade to Astro v6

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,13 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: posts.map((post) => {
+			const [lang, ...slugParts] = post.id.split('/');
+			const slug = slugParts.join('/').replace(/\.(md|mdx)$/, '');
+			return {
+				...post.data,
+				link: `/${lang === 'en' ? '' : lang + '/'}blog/${slug}/`,
+			};
+		}),
 	});
 }
